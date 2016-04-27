194e9d6fa44de1811df30258a6a1314443d60c63
Anthony Ferlazzo <aferlazzo@gmail.com>
2016-04-26 19:45:12 -0700

    Step one of routing is working between component views

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,27 @@
 import {Component} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+
+import {FirstComponent}		from './first-page/first.component';
+import {SecondComponent}	from './second-page/second.component';
+import {ThirdComponent}		from './third-page/third.component';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `<h1>Angular 2 Router Reference App</h1>
+			<nav>
+				<a [routerLink]="['FirstPart']">show first component</a>
+				<a [routerLink]="['SecondPart']">show second component</a>
+				<a [routerLink]="['ThirdPart']">show third component</a>
+			</nav>
+			<router-outlet></router-outlet>
+		`,
+		directives: [ROUTER_DIRECTIVES]
 })
+
+//simultaneously create a router and add its routes
+@RouteConfig([
+	{path:'/first', name: 'FirstPart', component: FirstComponent, useAsDefault: true},
+	{path:'/second', name: 'SecondPart', component: SecondComponent},
+	{path:'/third', name: 'ThirdPart', component: ThirdComponent}
+])
 export class AppComponent { }
